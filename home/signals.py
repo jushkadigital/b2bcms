24c93da0bc34985065a67f049418df1b6f60da08
chamba <urgosxd@gmail.com>
2024-07-23 16:39:01 -0700

    aoe

--- home/signals.py
+++ home/signals.py
@@ -1,18 +1,17 @@
 
 from wagtail.models import ValidationError
 from wagtail.signals import page_published
+from paquete.models import Paquete 
 import requests
 
 
 # Let everyone know when a new page is published
-def send_to_vercel(sender, **kwargs):
+def send_to_vercel_Paquete(sender, **kwargs):
     instance = kwargs['instance']
-    url = 'https://api.vercel.com/v1/integrations/deploy/prj_7x5hHOUPrZhjTqQkCtvWoflxhW5W/9cv7iNWwRv'
-    values = {
-    }
-    # response = requests.get(url)
-    # print(response)
-    print("gaaa")
+    print(dir(instance))
+    print(instance.get_default_locale)
+    print(instance.get_default_locale())
+    # url = 'https://api.vercel.com/v1/integrations/deploy/prj_7x5hHOUPrZhjTqQkCtvWoflxhW5W/9cv7iNWwRv'
 
 # Register a receiver
-page_published.connect(send_to_vercel)
+page_published.connect(send_to_vercel_Paquete,sender=Paquete)
