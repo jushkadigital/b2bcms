3ab092b4ba7d2f5aa6f53a6e93d5762d1315d456
chamba <urgosxd@gmail.com>
2024-07-24 13:31:10 -0700

    aoe

--- home/signals.py
+++ home/signals.py
@@ -8,17 +8,12 @@ import requests
 # Let everyone know when a new page is published
 def send_to_vercel_Paquete(sender, **kwargs):
     instance = kwargs['instance']
-    print(dir(instance))
-    print(instance.get_default_locale())
-    print(type(sender).__name__)
-    print(sender.__name__)
     # print(sender.content_type())
     print(dir(sender))
-    values = {"sender":sender.__name__,"lng":instance.get_default_locale() }
-    url = "https://www.perudestinoseguro.com/api/isr"
-    response = requests.post(url,values)
+    values = {"sender":sender.__name__,"lng":instance.get_default_locale().__str__() }
+    url = "https://www.perudestinoseguro.com/api/isr/"
+    response = requests.post(url,json=values)
 
-    
     # url = 'https://api.vercel.com/v1/integrations/deploy/prj_7x5hHOUPrZhjTqQkCtvWoflxhW5W/9cv7iNWwRv'
 
 # Register a receiver
