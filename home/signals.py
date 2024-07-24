35267ef29d83b0985ce4f726301d52424e12f8d4
chamba <urgosxd@gmail.com>
2024-07-24 14:09:05 -0700

    aoe

--- home/signals.py
+++ home/signals.py
@@ -2,6 +2,7 @@
 from wagtail.models import ValidationError
 from wagtail.signals import page_published
 from paquete.models import Paquete 
+from tour.models import Tour 
 import requests
 
 
@@ -16,5 +17,17 @@ def send_to_vercel_Paquete(sender, **kwargs):
 
     # url = 'https://api.vercel.com/v1/integrations/deploy/prj_7x5hHOUPrZhjTqQkCtvWoflxhW5W/9cv7iNWwRv'
 
+# Let everyone know when a new page is published
+def send_to_vercel_Tour(sender, **kwargs):
+    instance = kwargs['instance']
+    # print(sender.content_type())
+    print(dir(sender))
+    values = {"sender":sender.__name__,"lng":instance.get_default_locale().__str__(),"destino":  instance.tourDestino.__str__().lower() }
+    url = "https://www.perudestinoseguro.com/api/isr/"
+    response = requests.post(url,json=values)
+
+    # url = 'https://api.vercel.com/v1/integrations/deploy/prj_7x5hHOUPrZhjTqQkCtvWoflxhW5W/9cv7iNWwRv'
+
 # Register a receiver
 page_published.connect(send_to_vercel_Paquete,sender=Paquete)
+page_published.connect(send_to_vercel_Tour,sender=Tour)
