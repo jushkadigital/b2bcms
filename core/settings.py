9a4598ba8877a7dab5ca61068d0474ccd184563c
chamba <urgosxd@gmail.com>
2024-08-01 07:59:59 -0700

    aoe

--- core/settings.py
+++ core/settings.py
@@ -15,7 +15,7 @@ import os
 
 
 # MYURLFRONT = "https://www.perudestinoseguro.com"
-MYURLFRONT = "https://www.vps.perudestinoseguro.com"
+MYURLFRONT = "https://www.perudestinoseguro.com"
 
 CLOUDINARY_STORAGE = {
     # 'CLOUD_NAME': 'denkdx0za',  # required
