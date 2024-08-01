7675e5672501ee51859530bc343d9dc3a0d7e315
chamba <urgosxd@gmail.com>
2024-08-01 14:39:19 -0700

    aoe

--- paquete/models.py
+++ paquete/models.py
@@ -40,7 +40,6 @@ class CustomValidateForm(WagtailAdminPageForm):
     def clean(self):
         cleaned_data = super().clean()
 
-        print(cleaned_data)
 
         for key,val in (self.formsets.items()):
             if cleaned_data.get("action-publish") and len(val.forms) == 0 and key != "comments":
