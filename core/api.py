2199c13f38058a98c9ff60cd485c6709282bb218
chamba <urgosxd@gmail.com>
2024-08-25 17:57:11 -0700

    aoe

--- core/api.py
+++ core/api.py
@@ -9,6 +9,12 @@ from wagtail.images.api.v2.views import ImagesAPIViewSet
 
 from salidasGrupales.models import SalidasGrupales
 from tour.models import Tour
+from wagtail.api.v2.utils import (
+    BadRequestError,
+    page_models_from_string,
+)
+from wagtail.models import Page 
+
 # Create the router. "wagtailapi" is the URL namespace
 
 api_router = WagtailAPIRouter('wagtailapi')
@@ -28,16 +34,136 @@ class CustomTourApiViewSet(PagesAPIViewSet):
     renderer_classes = [JSONRenderer]
     name = "tour"
     model = Tour 
+    known_query_parameters = BaseAPIViewSet.known_query_parameters.union(
+        [
+            "type",
+            "child_of",
+            "ancestor_of",
+            "descendant_of",
+            "translation_of",
+            "locale",
+            "site",
+            "sss",
+            "sender"
+        ]
+    )
+    def get_queryset(self):
+        request = self.request
 
-class CustomTourApiViewSet(PagesAPIViewSet):
-    renderer_classes = [JSONRenderer]
-    name = "tour"
-    model = Tour 
+        # Allow pages to be filtered to a specific type
+        try:
+            models_type = request.GET.get("type", None)
+            models = models_type and page_models_from_string(models_type) or []
+        except (LookupError, ValueError):
+            raise BadRequestError("type doesn't exist")
+
+        if not models:
+            print("here")
+            if self.model == Page:
+                return self.get_base_queryset()
+            else:
+                algoParameter = request.GET.get("sss",None)
+                if algoParameter is None:
+                    return self.model.objects.filter(
+                    pk__in=self.get_base_queryset().values_list("pk", flat=True)
+                )
+                else:
+                    try:
+                        senderId = request.GET.get("sender",None)
+                        senderId = int(senderId)
+                    except (LookupError, ValueError):
+                        raise BadRequestError("sender doesn't exist")
+                    if algoParameter == "basic":
+                        return self.model.objects.filter(
+                        pk__in=self.get_base_queryset().exclude(pk=senderId).values_list("pk", flat=True)
+                        ).order_by('?')[:4]
+
+                    else:
+                        return self.model.objects.filter(
+                        pk__in=self.get_base_queryset().values_list("pk", flat=True)
+                        )
+
+
+        elif len(models) == 1:
+            # If a single page type has been specified, swap out the Page-based queryset for one based on
+            # the specific page model so that we can filter on any custom APIFields defined on that model
+            return models[0].objects.filter(
+                pk__in=self.get_base_queryset().values_list("pk", flat=True)
+            )
+
+        else:  # len(models) > 1
+            return self.get_base_queryset().type(*models)
+
+
+# class CustomTourApiViewSet(PagesAPIViewSet):
+#     renderer_classes = [JSONRenderer]
+#     name = "tour"
+#     model = Tour 
 
 class CustomPaqueteApiViewSet(PagesAPIViewSet):
     renderer_classes = [JSONRenderer]
     name = "paquete"
     model = Paquete
+    known_query_parameters = BaseAPIViewSet.known_query_parameters.union(
+        [
+            "type",
+            "child_of",
+            "ancestor_of",
+            "descendant_of",
+            "translation_of",
+            "locale",
+            "site",
+            "sss",
+            "sender"
+        ]
+    )
+    def get_queryset(self):
+        request = self.request
+
+        # Allow pages to be filtered to a specific type
+        try:
+            models_type = request.GET.get("type", None)
+            models = models_type and page_models_from_string(models_type) or []
+        except (LookupError, ValueError):
+            raise BadRequestError("type doesn't exist")
+
+        if not models:
+            print("here")
+            if self.model == Page:
+                return self.get_base_queryset()
+            else:
+                algoParameter = request.GET.get("sss",None)
+                if algoParameter is None:
+                    return self.model.objects.filter(
+                    pk__in=self.get_base_queryset().values_list("pk", flat=True)
+                )
+                else:
+                    try:
+                        senderId = request.GET.get("sender",None)
+                        senderId = int(senderId)
+                    except (LookupError, ValueError):
+                        raise BadRequestError("sender doesn't exist")
+                    if algoParameter == "basic":
+                        return self.model.objects.filter(
+                        pk__in=self.get_base_queryset().exclude(pk=senderId).values_list("pk", flat=True)
+                        ).order_by('?')[:1]
+
+                    else:
+                        return self.model.objects.filter(
+                        pk__in=self.get_base_queryset().values_list("pk", flat=True)
+                        )
+
+
+        elif len(models) == 1:
+            # If a single page type has been specified, swap out the Page-based queryset for one based on
+            # the specific page model so that we can filter on any custom APIFields defined on that model
+            return models[0].objects.filter(
+                pk__in=self.get_base_queryset().values_list("pk", flat=True)
+            )
+
+        else:  # len(models) > 1
+            return self.get_base_queryset().type(*models)
+
 
 class CustomInicioApiViewSet(PagesAPIViewSet):
     renderer_classes = [JSONRenderer]
